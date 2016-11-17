9d9ed4148c00e35980fbe4b4b4ccadfe0f19a246
30bySixty <tshestopalova@gmail.com>
2016-11-17 12:41:06 -0600

    configured mailer_sender in devise.rb

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -12,7 +12,8 @@ Devise.setup do |config|
   # Configure the e-mail address which will be shown in Devise::Mailer,
   # note that it will be overwritten if you use your own mailer class
   # with default "from" parameter.
-  config.mailer_sender = 'please-change-me-at-config-initializers-devise@example.com'
+#  config.mailer_sender = 'please-change-me-at-config-initializers-devise@example.com'
+  config.mailer_sender = 'hello@letsgifton.com'
   
   config.omniauth :facebook, ENV["FACEBOOK_APP_ID"], ENV["FACEBOOK_APP_SECRET"],
   scope: 'email,public_profile', info_fields: 'email, first_name, last_name, gender, age_range'
