a329a51a79369f38980dcd19b7115f19a5d924a3
30bySixty <tshestopalova@gmail.com>
2016-11-16 20:22:57 -0600

    write feature spec test for showing profile information via dashboard

---
+++ spec/features/dashboard/profile/showing_profile_spec.rb
@@ -0,0 +1,23 @@
+require 'rails_helper'
+
+RSpec.feature "Showing Profile" do 
+  before do
+    @user = FactoryGirl.create(:user)
+    @user2 = FactoryGirl.create(:user)
+    @new_first_name = Faker::Name::first_name
+    @new_last_name = Faker::Name::last_name
+    @new_about_me = Faker::Hipster::sentence
+  end
+  
+  scenario "Logged-in user can see her profile information via the dashboard", js:true do
+    login_as(@user, :scope => :user)
+    visit(dashboard_path(@user))
+    click_link "Settings"
+    
+    expect(page).to have_content(@user.email)
+    expect(page).to have_link("Change Password")
+    expect(page).to have_content(@user.birthday)
+    expect(page).to have_content(@user.gender)
+    expect(page).to have_link("Deactivate Account")
+  end
+end
