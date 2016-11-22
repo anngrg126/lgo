c6144e1e5cc76ab3c48e6297ce4964a5c8ab3c7e
30bySixty <tshestopalova@gmail.com>
2016-11-22 14:02:38 -0600

    create notifications_email method and view

--- app/mailers/notifications_mailer.rb
+++ app/mailers/notifications_mailer.rb
@@ -1,2 +1,27 @@
 class NotificationsMailer < ApplicationMailer
+  default from: 'hello@letsgifton.com'
+  
+  def notifications_email(user)
+    @user = user
+    @new_notifications = Notification.where(user_id: @user.id, read: false, updated_at: (Time.now - 24.hours)..Time.now)
+    
+    if @new_notifications.count > 0
+      @notifiers = []
+      @new_notifications.each do |n|
+        @notifiers.push(User.find(n.notified_by_user_id).first_name)
+      end
+      @notifiers.uniq!
+      if @notifiers.count > 3
+        mail(
+          to: @user.email,
+          subject: '#{@new_notifications.count} new notifications from #{@notifiers.first(3).join(", ")}, and #{@notifiers.count-3} more'
+          )
+      else
+        mail(
+          to: @user.email,
+          subject: '#{@new_notifications.count} new notifications from #{@notifiers.to_sentence}'
+          )
+      end
+    end
+  end
 end
