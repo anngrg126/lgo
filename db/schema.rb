356500f0932d6e7d204d21390d8269d95cab851f
30bySixty <tshestopalova@gmail.com>
2016-11-23 11:18:19 -0600

    add user_preferences model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161119003442) do
+ActiveRecord::Schema.define(version: 20161123171451) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -157,6 +157,15 @@ ActiveRecord::Schema.define(version: 20161119003442) do
     t.index ["user_id"], name: "index_stories_on_user_id", using: :btree
   end
 
+  create_table "subscription_preferences", force: :cascade do |t|
+    t.integer  "user_id"
+    t.boolean  "daily_email"
+    t.boolean  "weekly_email"
+    t.datetime "created_at",   null: false
+    t.datetime "updated_at",   null: false
+    t.index ["user_id"], name: "index_subscription_preferences_on_user_id", using: :btree
+  end
+
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "",    null: false
     t.string   "encrypted_password",     default: "",    null: false
@@ -207,4 +216,5 @@ ActiveRecord::Schema.define(version: 20161119003442) do
   add_foreign_key "stories", "users", column: "admin_id"
   add_foreign_key "stories", "users", column: "author_id"
   add_foreign_key "stories", "users", column: "poster_id"
+  add_foreign_key "subscription_preferences", "users"
 end
