5f2215b598489c7fc476ef0af2a3911a1b9e38da
30bySixty <tshestopalova@gmail.com>
2016-11-26 17:17:13 -0600

    add comments to run mailer previews

--- spec/mailers/previews/notifications_mailer_preview.rb
+++ spec/mailers/previews/notifications_mailer_preview.rb
@@ -5,6 +5,7 @@ class NotificationsMailerPreview < ActionMailer::Preview
   end
   
   #preview:  http://localhost:3000/rails/mailers/notifications_mailer/daily_email
+  #Note: to enable preview, User#176 must have received a notification within the past 24 hours
   
   def admin_story_email
     NotificationsMailer.admin_story_email(Story.first)
