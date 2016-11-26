9b8cc0f10ce363d62e7e51f5f8751c41c7266af5
30bySixty <tshestopalova@gmail.com>
2016-11-26 17:25:58 -0600

    create TagCategory model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161123171451) do
+ActiveRecord::Schema.define(version: 20161126223706) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -164,6 +164,12 @@ ActiveRecord::Schema.define(version: 20161123171451) do
     t.datetime "created_at",                  null: false
     t.datetime "updated_at",                  null: false
     t.index ["user_id"], name: "index_subscription_preferences_on_user_id", using: :btree
+  end
+
+  create_table "tag_categories", force: :cascade do |t|
+    t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
