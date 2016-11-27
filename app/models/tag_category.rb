a91ca9fa90208e1c439174c1f2c410deaa187181
30bySixty <tshestopalova@gmail.com>
2016-11-26 20:56:55 -0600

    validate uniqueness of tag category and rename column to 'category' from 'name'

--- app/models/tag_category.rb
+++ app/models/tag_category.rb
@@ -1,3 +1,4 @@
 class TagCategory < ApplicationRecord
   has_many :tags
+  validates :category, uniqueness: true
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161127023723) do
+ActiveRecord::Schema.define(version: 20161127025214) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -177,9 +177,9 @@ ActiveRecord::Schema.define(version: 20161127023723) do
   end
 
   create_table "tag_categories", force: :cascade do |t|
-    t.string   "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string   "category"
   end
 
   create_table "tags", force: :cascade do |t|
