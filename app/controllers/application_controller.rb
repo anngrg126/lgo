31308393c84945bffeff2ae55242401886de030f
Sanyukta <sanyukta22@gmail.com>
2016-12-02 20:31:19 +0000

    Activate deactive user on login

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,6 +6,13 @@ class ApplicationController < ActionController::Base
   
   def after_sign_in_path_for(resource)
 #    request.env['omniauth.origin'] || stored_location_for(resource) || root_path
-    super resource
+    @user = current_user
+    if @user.deactivated_at != nil
+      @user.deactivated_at = nil
+      @user.save
+      # activate_stories(@user)
+      # activate_comments(@user)
+    end
+    super resource   
   end
 end
