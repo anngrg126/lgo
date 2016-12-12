8b496b673fe058010bcc450539436da39a3eb8d0
30bySixty <tshestopalova@gmail.com>
2016-12-12 15:54:40 -0600

    scope tags by TagCategory

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -5,4 +5,12 @@ class Tag < ApplicationRecord
   
   has_many :classifications
   has_many :stories, through: :classifications
+  
+  scope :relationship, -> { joins(:tag_category).where(tag_categories: { category: 'Relationship' }) }
+  scope :occasion, -> { joins(:tag_category).where(tag_categories: { category: 'Occasion' }) }
+  scope :type, -> { joins(:tag_category).where(tag_categories: { category: 'Type' }) }
+  scope :interests, -> { joins(:tag_category).where(tag_categories: { category: 'Interests' }) }
+  scope :recipient, -> { joins(:tag_category).where(tag_categories: { category: 'To_recipient' }) }
+  scope :gifton_reaction, -> { joins(:tag_category).where(tag_categories: { category: 'Gifton_reaction' }) }
+  scope :collection, -> { joins(:tag_category).where(tag_categories: { category: 'Collection' }) }
 end
