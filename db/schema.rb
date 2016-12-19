79750cfc9ecd939f5a01e4f431755b5a0888761e
30bySixty <tshestopalova@gmail.com>
2016-12-18 22:20:21 -0600

    add anonymous boolean to User model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161207010712) do
+ActiveRecord::Schema.define(version: 20161219041844) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -225,6 +225,7 @@ ActiveRecord::Schema.define(version: 20161207010712) do
     t.string   "fbimage"
     t.string   "slug"
     t.datetime "deactivated_at"
+    t.boolean  "anonymous"
     t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
     t.index ["slug"], name: "index_users_on_slug", unique: true, using: :btree
