2ead28056a07349ad4239a0dc03dc751a514e0ad
30bySixty <tshestopalova@gmail.com>
2016-12-25 10:16:03 -0600

    replace signed_in_user helper method with user.display_name

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,13 +1,13 @@
 module ApplicationHelper
-  def signed_in_user
-    unless current_page? registration_step_path(:basic_details)
-      html = <<-HTML
-<div class="navbar-text">Signed in as #{current_user.email}.</div>
-HTML
-      html.html_safe
-    end
-  end
-  
+#  def signed_in_user
+#    unless current_page? registration_step_path(:basic_details)
+#      html = <<-HTML
+#<div class="navbar-text">Signed in as #{current_user.email}.</div>
+#HTML
+#      html.html_safe
+#    end
+#  end
+#  
   def user_notifications_link
     unless current_user
       link_to "My Notifications", new_user_session_path, class: "hide-for-small-only inactive-link"
