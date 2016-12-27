c23f2710dc9c1e95b46a76dd5650d94af92972e3
30bySixty <tshestopalova@gmail.com>
2016-12-27 11:46:30 +0100

    update notifications link helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -13,7 +13,11 @@ module ApplicationHelper
       link_to "My Notifications", new_user_session_path, class: "hide-for-small-only inactive-link"
     else
       unless Notification.where(user_id: current_user.id, read: false).count == 0
-        link_to "Notifications: #{Notification.where(user_id: current_user.id, read: false).count}", notifications_dashboard_path(current_user)
+#        link_to "My Notifications (#{Notification.where(user_id: current_user.id, read: false).count})", notifications_dashboard_path(current_user)
+        link_to notifications_dashboard_path(current_user) do
+          concat "My Notifications ".html_safe
+          concat "<span class='notifications_count'>(#{@user.notifications.select{|n| n.read == false}.length})</span>".html_safe
+        end
       end
     end
   end
