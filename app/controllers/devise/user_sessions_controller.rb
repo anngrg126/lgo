864a4ca60f3b81c290a67336acb9147d54409ed0
Sanyukta <sanyukta22@gmail.com>
2017-01-20 01:48:54 +0000

    Resolved error on logging out already logged in sessions

--- app/controllers/devise/user_sessions_controller.rb
+++ app/controllers/devise/user_sessions_controller.rb
@@ -3,9 +3,17 @@ class Devise::UserSessionsController < Devise::SessionsController
     before_action :set_tags, only: [:show, :index, :new, :edit]
     
     def destroy
-        @user = current_user
-        log_user_sign_out(@user)
+      if current_user
+        if UserSessionLog.where(user_id: current_user.id).any?
+          @user = current_user
+          log_user_sign_out(@user)
+          super
+        else
+          super
+        end
+      else
         super
+      end
     end
 
     def log_user_sign_out(resource)
