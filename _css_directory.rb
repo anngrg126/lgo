62567585eaeba3e06ee513e6f2d512113654c936
30bySixty <tshestopalova@gmail.com>
2017-01-25 17:03:55 -0600

    implement improved front end file infrastructure

---
+++ _css_directory.rb
@@ -0,0 +1,21 @@
+#application.scss
+#|-- views
+#|   |-- shared
+#|   |   |--footer.scss
+#|   |   |--navbar.scss
+#|   |   |--layout.scss
+#|   |-- stories.scss
+#|   |-- dashboards.scss
+#|   |-- ...
+#|-- globals
+#|   |-- _colors.scss
+#|   |-- _typography.scss
+#|   |-- _buttons.scss
+#|   |-- _settings.scss #from foundation
+#|   |-- ....
+#|   |-- components
+#|   |   |-- _story_poster.scss
+#|   |   |-- _user_thumbnail.scss
+#|   |   |-- ...
+#|-- vendor
+#|   |-- foundation_and_overrides.scss
