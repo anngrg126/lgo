5d1047fa7d87046001404699faa249e9962544b0
30bySixty <tshestopalova@gmail.com>
2017-03-08 17:37:59 -0600

    remove old tests

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -9,11 +9,11 @@ RSpec.describe PagesController, type: :controller do
     end
   end
 
-  describe "GET #privacy" do
-    it "returns http success" do
-      get :privacy
-      expect(response).to have_http_status(:success)
-    end
-  end
+#  describe "GET #privacy" do
+#    it "returns http success" do
+#      get :privacy
+#      expect(response).to have_http_status(:success)
+#    end
+#  end
 
 end
